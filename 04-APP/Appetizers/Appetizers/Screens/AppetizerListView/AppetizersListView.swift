888351a723adc507ca933a579aff7c79dce8dcdc
luksinocencio <luksinocencio@gmail.com>
2025-06-11 12:58:47 -0300

    *

--- 04-APP/Appetizers/Appetizers/Screens/AppetizerListView/AppetizersListView.swift
+++ 04-APP/Appetizers/Appetizers/Screens/AppetizerListView/AppetizersListView.swift
@@ -2,19 +2,34 @@ import SwiftUI
 
 struct AppetizersListView: View {
     @StateObject var viewModel = AppetizersListViewModel()
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            isShowingDetail = true
+                            selectedAppetizer = appetizer
+                        }
                 }
                 .listStyle(.inset)
                 .navigationTitle("🍟 Appetizers")
                 .scrollIndicators(.hidden)
+                .disabled(isShowingDetail)
             }
             .onAppear {
                 viewModel.getAppetizers()
+            }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            
+            if isShowingDetail {
+                AppetizerDetailView(
+                    appetizer: selectedAppetizer!,
+                    isShowingDetail: $isShowingDetail
+                )
             }
             
             if viewModel.isLoading {

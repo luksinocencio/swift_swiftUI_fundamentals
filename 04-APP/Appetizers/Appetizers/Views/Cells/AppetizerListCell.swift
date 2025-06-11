79a2799877320341330279a5d9c0c29f5050dba1
luksinocencio <luksinocencio@gmail.com>
2025-06-11 11:56:54 -0300

    *

--- 04-APP/Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
+++ 04-APP/Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
@@ -5,11 +5,11 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            Image("asian-flank-steak")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 90)
                 .cornerRadius(8)
+            
             VStack(alignment: .leading) {
                 Text(appetizer.name)
                     .font(.title2)
